e9f4aefb6e09db7c76a82bd94152c582123d73b3
EGOBA KELVIN <egobakelvin@gmail.com>
2024-08-07 16:39:01 +0100

    update on the frontend

--- frontend-store/src/utils/Index.js
+++ frontend-store/src/utils/Index.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  // baseURL: "https://coperative.onrender.com/api/v1"
+ baseUrl : process.env.VUE_APP_API_BASE_URL || 'http://localhost:8000'
+ 
+
 });
 
 // Add a request interceptor
@@ -11,7 +12,7 @@ axiosInstance.interceptors.request.use(
   config => {
     // Retrieve the token from localStorage
     const token = localStorage.getItem('token');
-    console.log()
+
 
     // If the token exists, add it to the Authorization header
     if (token) {
